dad8156cb6eadbe48b9cfe1af94ef2a0bbf6790d
ericatsu <ekaatsu@st.ug.edu.gh>
2023-05-03 20:52:45 +0000

    ✅ 415

--- src/main/java/com/lester/spring_api/student/StudentController.java
+++ src/main/java/com/lester/spring_api/student/StudentController.java
@@ -1,6 +1,5 @@
 package com.lester.spring_api.student;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentController {
     private final StudentService studentService;
 
-    @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
--- src/main/java/com/lester/spring_api/student/StudentService.java
+++ src/main/java/com/lester/spring_api/student/StudentService.java
@@ -1,7 +1,6 @@
 package com.lester.spring_api.student;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,7 +8,6 @@ public class StudentService {
     
     private final StudentRepository studentRepository;
     
-    @Autowired
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
