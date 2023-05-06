584760aecc382e34fda85f86d030feda89d596af
ericatsu <ekaatsu@st.ug.edu.gh>
2023-05-06 16:53:51 +0000

    front

--- src/main/java/com/lester/spring_api/SpringApiApplication.java
+++ src/main/java/com/lester/spring_api/SpringApiApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringApiApplication.class, args);
 	}
